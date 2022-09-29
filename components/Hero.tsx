a94841fecda7aa15be2c2393088ecfee97cb9b3d
masahisazumi <m.hisazumi@gmail.com>
2022-09-29 21:03:58 +0900

    hero animation

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Cursor, useTypewriter } from 'react-simple-typewriter'
+import BackgroundCircles from './BackgroundCircles'
 
 type Props = {}
 
@@ -17,8 +18,12 @@ export default function Hero({}: Props) {
   })
 
   return (
-    <div>
-      <p>{text}</p>
+    <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
+      <BackgroundCircles />
+      <h1>
+        <span>{text}</span>
+        <Cursor cursorColor='#F7A0BA' />
+      </h1>
     </div>
   )
 }
